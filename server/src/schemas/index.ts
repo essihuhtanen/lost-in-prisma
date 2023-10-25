a604fbc6463b0d52cea1658f38bc9fabe61035c7
Essi <essi.huhtanen@tuni.fi>
2023-10-25 17:05:40 +0300

    Export all schemas from index.ts

---
+++ server/src/schemas/index.ts
@@ -0,0 +1,12 @@
+import { CategorySchema, Category } from './category.schema';
+import { IngredientSchema, Ingredient } from './ingredient.schema';
+import { RecipeSchema, Recipe } from './recipe.schema';
+
+export {
+  Category,
+  Ingredient,
+  Recipe,
+  RecipeSchema,
+  IngredientSchema,
+  CategorySchema,
+};
