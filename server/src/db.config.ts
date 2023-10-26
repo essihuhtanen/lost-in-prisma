9cca2f7cd506fdd0b2e710ce5addea0c4dd8378c
Essi <essi.huhtanen@tuni.fi>
2023-10-26 09:02:33 +0300

    Add actual url for test db

--- server/src/db.config.ts
+++ server/src/db.config.ts
@@ -2,9 +2,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const MONGO_PROD = process.env.MONGO_URI;
-const MONGO_MOCK = "don't know yet";
+const MONGO_TEST = process.env.MONGO_URI_TEST;
 
 export function getDatabaseUri() {
   console.log(process.env.NODE_ENV);
-  return process.env.NODE_ENV === 'test' ? MONGO_MOCK : MONGO_PROD;
+  return process.env.NODE_ENV === 'test' ? MONGO_TEST : MONGO_PROD;
 }
